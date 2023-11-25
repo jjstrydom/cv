82d8c0432a13f1d86d44add8cb82756640439a62
Jurgen Strydom <jurgen.strydom@gmail.com>
2023-11-25 15:59:25 +0200

    wrapping to get the styles

--- build_cv.py
+++ build_cv.py
@@ -9,31 +9,28 @@ prefix = """\
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-
 	<title>CV</title>
-
 	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" media="all" /> 
-	<link rel="stylesheet" type="text/css" href="resume.css" media="all" />
-
+	<link rel="stylesheet" type="text/css" href="cv.css" media="all" />
 </head>
 <body>
-
 <div id="doc2" class="yui-t7">
 	<div id="inner">
 """
 
 postfix = """\
 	</div><!-- // inner -->
-
-
 </div><!--// doc -->
-
-
 </body>
 </html>
 """
 
 soup = BeautifulSoup(prefix + html + postfix, features="html.parser")
+
+wrap = soup.new_tag('div', **{"id": "hd"})
+wrapper = soup.new_tag('div', **{"class": "yui-gc"})
+wrapper2 = soup.new_tag('div', **{"class": "yui-u first"})
+soup.find('h1').wrap(wrapper2).wrap(wrapper).wrap(wrap)
 
 with open("cv.html","w") as html_file:
     html_file.write(soup.prettify())
